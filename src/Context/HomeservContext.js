e78b610b985ac27a43bdda9a81a926910aa22658
Ayush Rawat <97681088+ayushrawat-github@users.noreply.github.com>
2024-03-07 13:03:40 +0530

    change1

--- src/Context/HomeservContext.js
+++ src/Context/HomeservContext.js
@@ -4,7 +4,7 @@ const HomeServContext = createContext({
   Homeserv: [
     {
       ImgLink:
-        "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.unsplash.com/photo-1601841162542-956af38ba052?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       Name: "ICU",
       infoServ: [
         "hello this depart is for icu services here we provide one of services in sharanpur we have given this serv to thousand of people and in our hosipital we have best doctor",
